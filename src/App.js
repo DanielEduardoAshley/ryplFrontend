8bd23ffc0fe0a09264dc21aad71e4ebdd9686e28
daniel ashley <danielashley@pursuit.org>
2019-05-12 16:06:13 -0400

    Created contexts and component folders, npm installed axios react-router-dom bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      Hello World
-    </div>
-  );
+  const arr = [1, 2, 3, 4];
+  for (let i = 0; i < arr.length; i++) {
+    console.log(i);
+  }
+  return <div>Hello World</div>;
 }
 
 export default App;
---
+++ src/contexts/auth.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Auth = React.createContext(null);
+
+export default Auth;
