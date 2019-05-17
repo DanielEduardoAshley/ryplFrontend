b73cd6e1445c560a11f695f8aeee343edf034bb3
daniel ashley <danielashley@pursuit.org>
2019-05-16 20:39:34 -0400

    Video recorder works using webrtc and able to play firebase url

--- src/components/video.js
+++ src/components/video.js
@@ -1,11 +1,15 @@
 // import React from "react";
-// import firebase from "../firebase";
-// import { Player } from "video-react";
+import firebase from "../firebase";
+import { Player } from "video-react";
 // import "video.js/dist/video-js.css";
 // import VideoRecorder from "react-video-recorder";
 // import 'video.js/dist/video-js.css';
 import React from "react";
-import VideoRecorder from "react-video-recorder";
+// import  VideoRecorder, { handleVideoSelected } from "react-video-recorder";
+// import RecordRTC,{ stream, MediaStreamRecorder } from "recordrtc"
+// import MediaCapturer from 'react-multimedia-capture';
+// import ReactMediaRecorder from 'react-multimedia-capture';
+
 // import { onStopRecording } from "react-video-recorder";
 // class App extends React.Component{
 
@@ -118,24 +122,224 @@ import VideoRecorder from "react-video-recorder";
 //   }
 // }
 // const { videojs } = require("videojs-record");
+
 class Video extends React.Component {
-  componentDidMount() {
-    console.log();
+  state = {
+    blob: [],
+    url: ''
+  };
+   updatedBlob=(blobs)=>{
+    this.setstate({
+      blob : blobs
+    })
   }
-  render() {
-    return (
-      <VideoRecorder
-        onTurnOnCamera={e => {
-          console.log("here1");
-        }}
-        onStartRecording={e => {
-          console.log("here2");
-        }}
-        onRecordingComplete={(videoBlob, startedAt, thumbnailBlob, duration) =>
-          console.log("hello", videoBlob, startedAt, thumbnailBlob, duration)
+  
+   blobs = [];
+  componentDidMount() {
+    const blobUpdate=()=>{
+      this.setState({
+        blob: 'here'
+      })
+    }
+     
+   const updateBlob=(recordedBlobs)=>{
+    this.setState({
+      blob: (this.state.blob || []).concat(recordedBlobs)
+    })
+    }
+    
+
+    
+  
+    
+    var mediaSource = new MediaSource();
+    mediaSource.addEventListener("sourceopen", handleSourceOpen, false);
+    var mediaRecorder;
+    var recordedBlobs;
+    var sourceBuffer;
+
+    var gumVideo = document.querySelector("video#gum");
+    var recordedVideo = document.querySelector("video#recorded");
+
+    var recordButton = document.querySelector("button#record");
+    var playButton = document.querySelector("button#play");
+    var downloadButton = document.querySelector("button#download");
+    recordButton.onclick = toggleRecording;
+    playButton.onclick = play;
+    downloadButton.onclick = download;
+
+    // console.log(location.host);
+    // window.isSecureContext could be used for Chrome
+    // var isSecureOrigin = location.protocol === 'https:' ||
+    // location.host.includes('localhost');
+    // if (!isSecureOrigin) {
+    // alert('getUserMedia() must be run from a secure origin: HTTPS or localhost.' +
+    //   '\n\nChanging protocol to HTTPS');
+    // location.protocol = 'HTTPS';
+
+    var constraints = {
+      audio: true,
+      video: true
+    };
+
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(successCallback, errorCallback);
+
+    function successCallback(stream) {
+      console.log("getUserMedia() got stream: ", stream);
+      window.stream = stream;
+      gumVideo.srcObject = stream;
+    }
+
+    function errorCallback(error) {
+      console.log("navigator.getUserMedia error: ", error);
+    }
+
+    function handleSourceOpen(event) {
+      console.log("MediaSource opened");
+      sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vp8"');
+      console.log("Source buffer: ", sourceBuffer);
+    }
+
+    function handleDataAvailable(event) {
+      if (event.data && event.data.size > 0) {
+        recordedBlobs.push(event.data);
+      }
+    }
+
+    function handleStop(event) {
+      console.log("Recorder stopped: ", event);
+    }
+
+    function toggleRecording() {
+      if (recordButton.textContent === "Start Recording") {
+        startRecording();
+      } else {
+        stopRecording();
+        recordButton.textContent = "Start Recording";
+        playButton.disabled = false;
+        downloadButton.disabled = false;
+      }
+    }
+
+    // The nested try blocks will be simplified when Chrome 47 moves to Stable
+    function startRecording() {
+      var options = {
+        mimeType: "video/webm;codecs=vp9",
+        bitsPerSecond: 100000
+      };
+      recordedBlobs = [];
+      try {
+        mediaRecorder = new MediaRecorder(window.stream, options);
+      } catch (e0) {
+        console.log(
+          "Unable to create MediaRecorder with options Object: ",
+          options,
+          e0
+        );
+        try {
+          options = {
+            mimeType: "video/webm;codecs=vp8",
+            bitsPerSecond: 100000
+          };
+          mediaRecorder = new MediaRecorder(window.stream, options);
+        } catch (e1) {
+          console.log(
+            "Unable to create MediaRecorder with options Object: ",
+            options,
+            e1
+          );
+          try {
+            options = "video/mp4";
+            mediaRecorder = new MediaRecorder(window.stream, options);
+          } catch (e2) {
+            alert("MediaRecorder is not supported by this browser.");
+            console.error("Exception while creating MediaRecorder:", e2);
+            return;
+          }
         }
+      }
+      console.log(
+        "Created MediaRecorder",
+        mediaRecorder,
+        "with options",
+        options
+      );
+      recordButton.textContent = "Stop Recording";
+      playButton.disabled = true;
+      downloadButton.disabled = true;
+      mediaRecorder.onstop = handleStop;
+      mediaRecorder.ondataavailable = handleDataAvailable;
+      mediaRecorder.start(10); // collect 10ms of data
+      console.log("MediaRecorder started", mediaRecorder);
+    }
+
+    function stopRecording() {
+      mediaRecorder.stop();
+      console.log("Recorded Blobs: ", recordedBlobs);
+      // blobs = recordedBlobs;
+      updateBlob(recordedBlobs)
+      recordedVideo.controls = true;
+    }
+    
+    function play() {
+      var superBuffer = new Blob(recordedBlobs, { type: "video/webm" });
+      recordedVideo.src = window.URL.createObjectURL(superBuffer);
+      const url = window.URL.createObjectURL(superBuffer)
+      
+    }
+
+    function download() {
+      var blob = new Blob(recordedBlobs, { type: "video/webm" });
+      var url = window.URL.createObjectURL(blob);
+      var a = document.createElement("a");
+      a.style.display = "none";
+      a.href = url;
+      a.download = "test.webm";
+      document.body.appendChild(a);
+      a.click();
+      setTimeout(function() {
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+      }, 100);
+
+     
+    }
+  }
+  
+  handleFileStream = async e=> {
+    const superBuffer = new Blob(this.state.blob, { type: "video/webm" });
+      const url = window.URL.createObjectURL(superBuffer)
+        const root = firebase.storage().ref();
+        const newImage = root.child(`vids/${this.state.blob}`);
+    
+        try {
+          const snapshot = await newImage.put(superBuffer);
+          const urls = await snapshot.ref.getDownloadURL();
+          this.setState({
+            url: this.state.url.concat(urls)
+          });
+          console.log(url);
+        } catch (err) {
+          console.log(err);
+        }
+        console.log('hello')
+      };
+
+
+  render() {
+      
+    console.log(this.state)
+    return <>{/* <VideoRecorder/> */}
+    <button onClick={e=>this.handleFileStream(e)}>button</button>
+    <Player
+        playsInline
+        poster="https://bostoncrusaders.org/wp-content/uploads/2014/12/kid-sad-face-new-york-1r6di21.jpg"
+        src={this.state.url}
       />
-    );
+    </>;
   }
 }
+
 export default Video;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+
 // import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import Video from "./components/video";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Video/>, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
