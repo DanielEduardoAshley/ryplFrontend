c3b3da55f4f43be3ca18b09a50929888fab69b02
Abdel Oufkir <abdeloufkir@Pursuits-MacBook-Air-3.local>
2019-05-29 12:44:41 -0400

    version two of the category page

--- src/containers/category.js
+++ src/containers/category.js
@@ -66,6 +66,42 @@ class Category extends Component {
           vidUrl:
             "https://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/vids%2F%5Bobject%20Blob%5D?alt=media&token=57cd456c-b689-4a4f-8426-863eba9baa0dhttps://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/vids%2F%5Bobject%20Blob%5D?alt=media&token=ef00bea4-b2c2-48d8-9666-1f6e8aba80ad",
           responses: [
+            {
+              responseUrl:
+                "https://firebasestorage.googleapis.com/v0/b/cactus-338da.appspot.com/o/video1.mp4?alt=media&token=efa054c4-6edf-456c-b450-ffef9c3f634e"
+            },
+            {
+              responseUrl:
+                "https://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/vids%2F%5Bobject%20Blob%5D?alt=media&token=57cd456c-b689-4a4f-8426-863eba9baa0dhttps://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/vids%2F%5Bobject%20Blob%5D?alt=media&token=ef00bea4-b2c2-48d8-9666-1f6e8aba80ad"
+            },
+            {
+              responseUrl:
+                "https://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/vids%2F%5Bobject%20Blob%5D?alt=media&token=57cd456c-b689-4a4f-8426-863eba9baa0dhttps://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/vids%2F%5Bobject%20Blob%5D?alt=media&token=ef00bea4-b2c2-48d8-9666-1f6e8aba80ad"
+            }
+          ]
+        },
+        {
+          vidUrl:
+            "https://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/vids%2F%5Bobject%20Blob%5D?alt=media&token=57cd456c-b689-4a4f-8426-863eba9baa0dhttps://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/vids%2F%5Bobject%20Blob%5D?alt=media&token=ef00bea4-b2c2-48d8-9666-1f6e8aba80ad",
+          responses: [
+            {
+              responseUrl:
+                "https://firebasestorage.googleapis.com/v0/b/cactus-338da.appspot.com/o/video1.mp4?alt=media&token=efa054c4-6edf-456c-b450-ffef9c3f634e"
+            },
+            {
+              responseUrl:
+                "https://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/vids%2F%5Bobject%20Blob%5D?alt=media&token=57cd456c-b689-4a4f-8426-863eba9baa0dhttps://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/vids%2F%5Bobject%20Blob%5D?alt=media&token=ef00bea4-b2c2-48d8-9666-1f6e8aba80ad"
+            },
+            {
+              responseUrl:
+                "https://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/vids%2F%5Bobject%20Blob%5D?alt=media&token=57cd456c-b689-4a4f-8426-863eba9baa0dhttps://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/vids%2F%5Bobject%20Blob%5D?alt=media&token=ef00bea4-b2c2-48d8-9666-1f6e8aba80ad"
+            }
+          ]
+        },
+        {
+          vidUrl:
+            "https://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/vids%2F%5Bobject%20Blob%5D?alt=media&token=57cd456c-b689-4a4f-8426-863eba9baa0dhttps://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/vids%2F%5Bobject%20Blob%5D?alt=media&token=ef00bea4-b2c2-48d8-9666-1f6e8aba80ad",
+          responses: [
             // {
             //   responseUrl:
             //     "https://firebasestorage.googleapis.com/v0/b/cactus-338da.appspot.com/o/video1.mp4?alt=media&token=efa054c4-6edf-456c-b450-ffef9c3f634e"
@@ -128,6 +164,48 @@ class Category extends Component {
                       >
                         <source src={e.vidUrl} />;
                       </video>
+                    </div>
+                  </div>
+                  <div className="row flex_row">
+                    <div className="card_content">
+                      <h3>Title</h3>
+                      <h4 className="reactions">
+                        Reactions: {e.responses.length}
+                      </h4>
+                    </div>
+                  </div>
+                  <div className="row responses-row">
+                    <div className="video_responses  col-9">
+                      {e.responses.map((res, idx) => {
+                        return (
+                          <video
+                            className="response_container"
+                            autoplay={true}
+                            loop={false}
+                            muted=""
+                          >
+                            <source src={res.responseUrl} />;
+                          </video>
+                        );
+                      })}
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+
+            {/* return (
+                <div className="card">
+                  <div className="row flex_row">
+                    <div className="main-video ">
+                      <video
+                        className="video_container"
+                        autoplay={true}
+                        loop={false}
+                        muted=""
+                      >
+                        <source src={e.vidUrl} />;
+                      </video>
 
                       <div className="card_content">
                         <h3>Title</h3>
@@ -151,7 +229,7 @@ class Category extends Component {
                   </div>
                 </div>
               );
-            })}
+            })} */}
           </div>
         </div>
       </>
