7dba92589dc8d4deaea907b1308213c8bf5d2d56
Chuyun Huang <chuyunhuang@pursuit.org>
2019-06-02 09:35:59 -0400

    homepage videocards

--- src/components/videoCards.js
+++ src/components/videoCards.js
@@ -4,65 +4,106 @@ import { Animated } from "react-animated-css";
 
 const VideoCards = props => {
   return (
-    <Animated
-      animationIn="slideInLeft"
-      animationInDuration={2000}
-      animationOutDuration={1000}
-      isVisible={true}
-    >
-      <div className="videoCard-wrapper">
-        <div className="video-card">
-          <video className="video">
-            <source
-              type="video/mp4"
-              src="https://firebasestorage.googleapis.com/v0/b/cactus-338da.appspot.com/o/video2.mp4?alt=media&token=d4a19c46-9e7b-44b7-890b-c9f602a71452"
-            />
-          </video>
-          <div className="video-title">
-            <h2>Title 1</h2>
-          </div>
-          <button className="video-card-btn">See Thread</button>
+    <div className="videoCard-wrapper">
+      <div className="video-card">
+        <video
+          className="video"
+          controls
+          style={{ width: "450px", height: "300px" }}
+        >
+          <source
+            type="video/mp4"
+            src="https://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/IMG_1958.webm?alt=media&token=10c18634-5403-45b2-8ef8-59be99e7a73f"
+          />
+        </video>
+        <div className="video-title">
+          <h2>David and Helen</h2>
         </div>
-
-        <div className="video-card">
-          <video className="video">
-            <source type="video/mp4" src="https://youtu.be/zcaskjhhXWQ" />
-          </video>
-          <div className="video-title">
-            <h2>Title 2</h2>
-          </div>
-          <button className="video-card-btn">See Thread</button>
-        </div>
-
-        <div className="video-card">
-          <video className="video">
-            <source type="video/mp4" src="https://youtu.be/zcaskjhhXWQ" />
-          </video>
-          <div className="video-title">
-            <h2>Title 3</h2>
-          </div>
-          <button className="video-card-btn">See Thread</button>
-        </div>
-        <div className="video-card">
-          <video className="video">
-            <source type="video/mp4" src="https://youtu.be/zcaskjhhXWQ" />
-          </video>
-          <div className="video-title">
-            <h2>Title 4</h2>
-          </div>
-          <button className="video-card-btn">See Thread</button>
-        </div>
-        <div className="video-card">
-          <video className="video">
-            <source type="video/mp4" src="https://youtu.be/zcaskjhhXWQ" />
-          </video>
-          <div className="video-title">
-            <h2>Title 5</h2>
-          </div>
-          <button className="video-card-btn">See Thread</button>
-        </div>
+        <button className="video-card-btn">See Thread</button>
       </div>
-    </Animated>
+
+      <div className="video-card">
+        <video
+          className="video"
+          controls
+          style={{ width: "450px", height: "300px" }}
+        >
+          <source
+            type="video/mp4"
+            src="https://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/userVideos%2Fe37ec800-83ca-11e9-a38a-e1a5c2c55deb?alt=media&token=516917a5-8a6c-4975-9e4e-a5f95ea1f1a5"
+          />
+        </video>
+        <div className="video-title">
+          <h2>Dan testing</h2>
+        </div>
+        <button className="video-card-btn">See Thread</button>
+      </div>
+
+      <div className="video-card">
+        <video
+          className="video"
+          controls
+          style={{ width: "450px", height: "300px" }}
+        >
+          <source
+            type="video/mp4"
+            src="https://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/userVideos%2F26d9cf90-83c7-11e9-b4f2-658cfa9dbd31?alt=media&token=357c8aaf-55e8-44ef-9db4-8f7662abcfa4"
+          />
+        </video>
+        <div className="video-title">
+          <h2>Dan still testing..</h2>
+        </div>
+        <button className="video-card-btn">See Thread</button>
+      </div>
+      <div className="video-card">
+        <video
+          className="video"
+          controls
+          style={{ width: "450px", height: "300px" }}
+        >
+          <source
+            type="video/mp4"
+            src="https://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/test%2Ftest.mp4?alt=media&token=ef354168-c799-4b95-aa5b-e3c2076a8f67"
+          />
+        </video>
+        <div className="video-title">
+          <h2>Title 4</h2>
+        </div>
+        <button className="video-card-btn">See Thread</button>
+      </div>
+      <div className="video-card">
+        <video
+          className="video"
+          controls
+          style={{ width: "450px", height: "300px" }}
+        >
+          <source
+            type="video/mp4"
+            src="https://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/IMG_1957.webm?alt=media&token=1ca7c2cb-d582-4854-873c-10f48c4c3484"
+          />
+        </video>
+        <div className="video-title">
+          <h2> Demo Practice</h2>
+        </div>
+        <button className="video-card-btn">See Thread</button>
+      </div>
+      <div className="video-card">
+        <video
+          className="video"
+          controls
+          style={{ width: "450px", height: "300px" }}
+        >
+          <source
+            type="video/mp4"
+            src="https://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/IMG_1957.webm?alt=media&token=1ca7c2cb-d582-4854-873c-10f48c4c3484"
+          />
+        </video>
+        <div className="video-title">
+          <h2>Title 5</h2>
+        </div>
+        <button className="video-card-btn">See Thread</button>
+      </div>
+    </div>
   );
 };
 
