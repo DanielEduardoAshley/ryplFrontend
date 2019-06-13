20c0b2d32407bdbca06c36badf5ed5d04ee9b9db
daniel ashley <danielashley@pursuit.org>
2019-06-12 21:01:18 -0400

    video page edits and upload

--- src/components/videoCards.js
+++ src/components/videoCards.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./videoCards.css";
+import { Player, ControlBar, PlayToggle } from 'video-react'
 
 const VideoCards = props => {
   console.log(props);
@@ -7,16 +8,36 @@ const VideoCards = props => {
   const videos = (
     <div className="videoCard-wrapper">
       {props.mostViewedVid.map((e, i) => {
+        console.log(e)
         return (
           <div className="video-card">
             <video
               className="video"
               controls
               style={{ width: "450px", height: "300px" }}
+              // poster={`${e.thumbnail_url}`}
+              
             >
               >
-              <source type="video/mp4" src={e.video_url} />
+              <source type="video/mp4" src={e.video_url} preload={"none"}/>
             </video>
+            {/* <img src={"https://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/uploads%2F21f30a80-8d10-11e9-b087-310e0299ff45%2Fy2mate.com%20-%20charlie_schmidts_keyboard_cat_the_original_J---aiyznGQ_360p.mp4?alt=media&token=d4ee9197-025e-4bf1-a083-04853b53d118"}></img> */}
+            {/* <Player
+              fluid={false}
+              
+              width={200}
+              height={200}
+              poster={e.thumbnail_url}
+             
+              src={e.video_url}
+              >
+               <ControlBar autoHide={false} disableDefaultControls={true} className={"my-class"}>
+             <PlayToggle />
+             </ControlBar>
+              
+              </Player>
+
+              /> */}
             <div className="video-title">
               <h2>{e.video_title}</h2>
             </div>
