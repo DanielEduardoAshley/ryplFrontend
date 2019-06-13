220b9dc71c8ff256c8003a4047ebce6df763f4e7
Daniel Ashley <43762291+DanielEduardoAshley@users.noreply.github.com>
2019-06-12 21:25:10 -0400

    Video category (#52)
    * fixed upload option
    
    * worked on videos and heroku
    
    * video page edits and upload

--- src/components/videoCards.js
+++ src/components/videoCards.js
@@ -1,22 +1,42 @@
 import React from "react";
 import "./videoCards.css";
-//trying user image
-import userImg from "./images/fakeuser.jpg";
+import { Player, ControlBar, PlayToggle } from 'video-react'
+
 const VideoCards = props => {
   const showLoading = <div>Loading</div>;
   const videos = (
     <div className="videoCard-wrapper">
       {props.mostViewedVid.map((e, i) => {
+        console.log(e)
         return (
           <div className="video-card">
             <video
               className="video"
               controls
               style={{ width: "450px", height: "300px" }}
+              // poster={`${e.thumbnail_url}`}
+              
             >
               >
-              <source type="video/mp4" src={e.video_url} />
+              <source type="video/mp4" src={e.video_url} preload={"none"}/>
             </video>
+            {/* <img src={"https://firebasestorage.googleapis.com/v0/b/rypl-acf62.appspot.com/o/uploads%2F21f30a80-8d10-11e9-b087-310e0299ff45%2Fy2mate.com%20-%20charlie_schmidts_keyboard_cat_the_original_J---aiyznGQ_360p.mp4?alt=media&token=d4ee9197-025e-4bf1-a083-04853b53d118"}></img> */}
+            {/* <Player
+              fluid={false}
+              
+              width={200}
+              height={200}
+              poster={e.thumbnail_url}
+             
+              src={e.video_url}
+              >
+               <ControlBar autoHide={false} disableDefaultControls={true} className={"my-class"}>
+             <PlayToggle />
+             </ControlBar>
+              
+              </Player>
+
+              /> */}
             <div className="video-title">
               <div className="user-wrapper">
                 <div className="user-image">
